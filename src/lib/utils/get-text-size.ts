0401fcd29fb72e1dd3a35949a979558d3c3de475
Andrey Volynkin <mail.avol@gmail.com>
2021-08-22 18:08:45 +0300

    Fix valuable text length detection

--- src/lib/utils/get-text-size.ts
+++ src/lib/utils/get-text-size.ts
@@ -1,8 +1,11 @@
+const NOT_VALUABLE_STRINGS_REGEXP = /&(?:lt|gt|amp|quot|apos);/gi;
 const NOT_VALUABLE_CHARS_REGEXP = /[^A-ZА-ЯЁ0-9]+/gi;
 
 export function getTextSize( text: string ): number
 {
-	const valuable = text.replace( NOT_VALUABLE_CHARS_REGEXP, '' );
+	const valuable = text
+		.replace( NOT_VALUABLE_STRINGS_REGEXP, '' )
+		.replace( NOT_VALUABLE_CHARS_REGEXP, '' );
 	
 	return valuable.length;
 }
